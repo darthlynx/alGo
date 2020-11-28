cd406f9dd6cdbaaebfb75c2cb9f4b4454e6ccdc0
Ihor Khlaponin <igor.boxmails@gmail.com>
2020-11-28 23:26:18 +0200

    task templates for timus and acmp

---
+++ readWriteFile/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+
+	inputs := readInputsFromFile("INPUT.TXT")
+	a, err := strconv.Atoi(inputs[0])
+	check(err)
+	b, err := strconv.Atoi(inputs[1])
+	check(err)
+	sum := a + b
+	fmt.Println(sum)
+	writeOutputToFile("OUTPUT.TXT", fmt.Sprintf("%d", sum))
+}
+
+func readInputsFromFile(fileName string) []string {
+	f, err := os.Open(fileName)
+	check(err)
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+	scanner.Scan()
+	line := scanner.Text()
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	inputs := strings.Fields(line)
+	fmt.Println(inputs)
+	return inputs
+}
+
+func writeOutputToFile(fileName, output string) {
+	f2, err := os.Create(fileName)
+	defer f2.Close()
+	f2.WriteString(output)
+	check(err)
+}
+
+func check(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
