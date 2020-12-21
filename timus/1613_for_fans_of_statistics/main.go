473a561f8609e68775ed899a87a94e788575580d
Ihor Khlaponin <igor.boxmails@gmail.com>
2020-12-21 17:12:33 +0200

    timus 1613: unfinished

---
+++ timus/1613_for_fans_of_statistics/main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+// https://acm.timus.ru/problem.aspx?space=1&num=1613&locale=en
+func main() {
+	// TODO
+	inputs := getInputLines()
+	n, err := strconv.Atoi(inputs[0]) // number of cities
+	checkError(err)
+	statistics := stringToIntArray(inputs[1], " ")
+	q, err := strconv.Atoi(inputs[2]) // requests number
+	checkError(err)
+
+	graph := make(map[int][]int, q)
+
+	for i := 3; i < len(inputs); i++ {
+		ii := stringToIntArray(inputs[i], " ")
+		graph[ii[len(ii)-1]] = ii[:len(ii)-1]
+	}
+	fmt.Println(n)
+	fmt.Println(statistics)
+	fmt.Println(graph)
+}
+
+func getInputLines() []string {
+	inputs := make([]string, 0)
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		// Scans a line from Stdin(Console)
+		scanner.Scan()
+		// Holds the string that scanned
+		text := scanner.Text()
+		if len(text) != 0 {
+			inputs = append(inputs, text)
+		} else {
+			break
+		}
+	}
+	return inputs
+}
+
+func stringToIntArray(str, sep string) []int {
+	ii := []int{}
+	stringArr := strings.Split(str, sep)
+	for i, _ := range stringArr {
+		tmp, err := strconv.Atoi(stringArr[i])
+		checkError(err)
+		ii = append(ii, tmp)
+	}
+	return ii
+}
+
+func checkError(err error) {
+	if err != nil {
+		log.Fatalf("failed writing to a file: %s", err)
+	}
+}
