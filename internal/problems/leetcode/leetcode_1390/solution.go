ab0ec7e48d639d2b633542bc9e9533bed881e3f2
Ihor Khlaponin <igor.boxmails@gmail.com>
2026-01-04 13:20:37 +0000

    leetcode 1390: sum of four divisors

--- internal/problems/leetcode/leetcode_1390/solution.go
+++ internal/problems/leetcode/leetcode_1390/solution.go
@@ -2,39 +2,37 @@ package leetcode1390
 
 // https://leetcode.com/problems/sum-of-four-divisors/
 //
-// Time Complexity: O(n * sqrt(m)) where n is the length of nums and m is the max number in nums
-// Space Complexity: O(n)
+// Time Complexity: O(n * sqrt(m)), where n is the length of nums and m is the maximum number in nums
+// Space Complexity: O(1)
 func sumFourDivisors(nums []int) int {
-	divisors := make(map[int]map[int]struct{})
-
-	for i := range nums {
-		if divisors[nums[i]] != nil {
-			continue
-		} else {
-			divisors[nums[i]] = getDivisors(nums[i])
-		}
-	}
-
-	count := 0
+	sum := 0
 	for _, num := range nums {
-		divs := divisors[num]
-		if len(divs) == 4 {
-			for key := range divs {
-				count += key
-			}
-		}
+		divisorsSum := getDivisorsSum(num)
+		sum += divisorsSum
 	}
-	return count
+
+	return sum
 }
 
-func getDivisors(num int) map[int]struct{} {
-	divisors := make(map[int]struct{})
+func getDivisorsSum(num int) int {
+	divisors := []int{}
 
 	for i := 1; i*i <= num; i++ {
 		if num%i == 0 {
-			divisors[i] = struct{}{}
-			divisors[num/i] = struct{}{}
+			divisors = append(divisors, i)
+			if i != num/i {
+				divisors = append(divisors, num/i)
+			}
+
+			if len(divisors) > 4 {
+				return 0
+			}
 		}
 	}
-	return divisors
+
+	if len(divisors) == 4 {
+		return divisors[0] + divisors[1] + divisors[2] + divisors[3]
+	}
+
+	return 0
 }
