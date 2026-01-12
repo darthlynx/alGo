c8a1d69f06e37786189681860601a554ab803322
Ihor Khlaponin <igor.boxmails@gmail.com>
2026-01-12 00:50:17 +0000

    leetcode 1266: minimum time visiting all points

--- internal/problems/leetcode/leetcode_1266/solution.go
+++ internal/problems/leetcode/leetcode_1266/solution.go
@@ -6,18 +6,13 @@ package leetcode1266
 // Space Complexity: O(1)
 func minTimeToVisitAllPoints(points [][]int) int {
 	time := 0
+
 	for i := 0; i < len(points)-1; i++ {
-		x := points[i][0]
-		y := points[i][1]
+		dx := abs(points[i+1][0] - points[i][0])
+		dy := abs(points[i+1][1] - points[i][1])
 
-		xx := points[i+1][0]
-		yy := points[i+1][1]
-
-		dx := abs(xx - x)
-		dy := abs(yy - y)
-
-		time += min(dx, dy)
-		time += max(dx, dy) - min(dx, dy)
+		// min(dx, dy) + (max(dx, dy) - min(dx, dy)) = max(dx, dy)
+		time += max(dx, dy)
 	}
 	return time
 }
