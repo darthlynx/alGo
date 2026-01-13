8856f39ca9633ab66f8ee009cf2d11d6824c159b
Ihor Khlaponin <igor.boxmails@gmail.com>
2026-01-13 23:02:16 +0000

    leetcode 3453: Separate Squares I

---
+++ internal/problems/leetcode/leetcode_3453/solution.go
@@ -0,0 +1,60 @@
+package leetcode3453
+
+import "math"
+
+// https://leetcode.com/problems/separate-the-squares-in-a-grid/
+//
+// Time Complexity: O(n * log(maxY - minY)) ~> O(n * 50) where n is the number of squares
+// Space Complexity: O(1)
+func separateSquares(squares [][]int) float64 {
+	var totalArea float64 = 0
+
+	minY := math.Inf(1)
+	maxY := math.Inf(-1)
+
+	for _, square := range squares {
+		y := float64(square[1])
+		length := float64(square[2])
+
+		minY = min(minY, y)
+		maxY = max(maxY, y+length)
+
+		totalArea += length * length
+	}
+
+	var halfArea float64 = totalArea / 2.0
+
+	// use 100 iterations for better precision
+	for range 50 {
+		var midY float64 = minY + (maxY-minY)/2
+		area := calculateArea(squares, midY, halfArea)
+		if area < halfArea {
+			minY = midY
+		} else {
+			maxY = midY
+		}
+	}
+
+	return minY
+}
+
+func calculateArea(squares [][]int, y float64, limit float64) float64 {
+	var below float64 = 0
+
+	for _, square := range squares {
+		y1 := float64(square[1])
+		length := float64(square[2])
+		y2 := y1 + length
+
+		if y >= y2 {
+			below += length * length
+		} else if y > y1 {
+			below += (y - y1) * length
+		}
+
+		if below > limit {
+			return below
+		}
+	}
+	return below
+}
