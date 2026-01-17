f0edd9617b4a90cb14a05ac2f76301cf46e0f359
Ihor Khlaponin <igor.boxmails@gmail.com>
2026-01-17 20:54:11 +0000

    leetcode 3047: find the largest area of square inside two rectangles

--- internal/problems/leetcode/leetcode_3047/solution.go
+++ internal/problems/leetcode/leetcode_3047/solution.go
@@ -5,32 +5,27 @@ package leetcode3047
 // Time Complexity: O(n^2) where n is the number of rectangles
 // Space Complexity: O(1)
 func largestSquareArea(bottomLeft [][]int, topRight [][]int) int64 {
-    n := len(bottomLeft)
+	n := len(bottomLeft)
 
-    var maxArea int64 = 0
-    for i := range n {
-        for j := range n {
-            if i == j {
-                continue
-            }
-yTop := min(topRight[i][1], topRight[j][1])
-            yBottom := max(bottomLeft[i][1], bottomLeft[j][1])
-            height := yTop - yBottom
-
-            xRight := min(topRight[i][0], topRight[j][0])
-            xLeft := max(bottomLeft[i][0], bottomLeft[j][0])
-            width := xRight - xLeft
+	var maxArea int64 = 0
+	for i := range n {
+		for j := range n {
+			if i == j {
+				continue
+			}
+			height := min(topRight[i][1], topRight[j][1]) - max(bottomLeft[i][1], bottomLeft[j][1])
+			width := min(topRight[i][0], topRight[j][0]) - max(bottomLeft[i][0], bottomLeft[j][0])
 
 			// No overlap
-            if height <= 0 || width <= 0 {
-                continue
-            }
+			if height <= 0 || width <= 0 {
+				continue
+			}
 
-            a := min(height, width)
-            area := int64(a * a)
-            maxArea = max(maxArea, area)
-        }
-    }
+			a := min(height, width)
+			area := int64(a * a)
+			maxArea = max(maxArea, area)
+		}
+	}
 
-    return maxArea
+	return maxArea
 }
