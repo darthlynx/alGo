b9148910803b0d4399b1f8669493e7754ce6bbc0
Ihor Khlaponin <igor.boxmails@gmail.com>
2026-03-20 00:59:03 +0000

    leetcode 3567: minimum absolute difference in sliding submatrix

--- internal/problems/leetcode/leetcode_3567/solution.go
+++ internal/problems/leetcode/leetcode_3567/solution.go
@@ -16,17 +16,20 @@ func minAbsDiff(grid [][]int, k int) [][]int {
 	cols := n - k + 1
 
 	result := make([][]int, rows)
+	for i := range result {
+		result[i] = make([]int, cols)
+	}
 
 	for row := 0; row < rows; row++ {
 		for col := 0; col < cols; col++ {
 			minAbsDiff := math.MaxInt
-			dist := make(map[int]struct{})
+			dist := make(map[int]struct{}, k*k)
 			for i := row; i < row+k; i++ {
 				for j := col; j < col+k; j++ {
 					dist[grid[i][j]] = struct{}{}
 				}
 			}
-			nums := []int{}
+			nums := make([]int, 0, len(dist))
 			for key := range dist {
 				nums = append(nums, key)
 			}
@@ -39,7 +42,7 @@ func minAbsDiff(grid [][]int, k int) [][]int {
 			if minAbsDiff == math.MaxInt {
 				minAbsDiff = 0
 			}
-			result[row] = append(result[row], minAbsDiff)
+			result[row][col] = minAbsDiff
 		}
 	}
 
