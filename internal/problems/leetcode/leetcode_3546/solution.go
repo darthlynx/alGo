ae3a93ac8c01f6119522ac318bb9cc5f3659e1b7
Ihor Khlaponin <igor.boxmails@gmail.com>
2026-03-25 00:34:54 +0000

    leetcode 3546: equal sum grid partition I

--- internal/problems/leetcode/leetcode_3546/solution.go
+++ internal/problems/leetcode/leetcode_3546/solution.go
@@ -3,50 +3,40 @@ package leetcode3546
 // https://leetcode.com/problems/equal-sum-grid-partition-i/
 //
 // Time Complexity: O(m*n).
-// Space Complexity: O(m+n).
+// Space Complexity: O(1).
 func canPartitionGrid(grid [][]int) bool {
-	m := len(grid)
-	n := len(grid[0])
+    m := len(grid)
+    n := len(grid[0])
 
-	total := 0
-	// sum per row
-	rows := make([]int, m)
-	for i := 0; i < m; i++ {
-		sum := 0
-		for j := 0; j < n; j++ {
-			sum += grid[i][j]
-		}
-		rows[i] = sum
-		total += sum
-	}
+    // total sum
+    total := 0
+    for r := 0; r < m; r++ {
+        for c := 0; c < n; c++ {
+            total += grid[r][c]
+        }
+    }
 
-	// check all horizontal cuts
-	currentSum := 0
-	for r := 0; r < m; r++ {
-		currentSum += rows[r]
-		if total-currentSum == currentSum {
-			return true
-		}
-	}
+    // check all horizontal cuts
+    topSum := 0
+    for r := 0; r < m-1; r++ {
+        for c := 0; c < n; c++ {
+            topSum += grid[r][c]
+        }
+        if topSum*2 == total {
+            return true
+        }
+    }
 
-	// calculate columns sums
-	cols := make([]int, n)
-	for j := 0; j < n; j++ {
-		sum := 0
-		for i := 0; i < m; i++ {
-			sum += grid[i][j]
-		}
-		cols[j] = sum
-	}
+    // check all vertical cuts
+    leftSum := 0
+    for c := 0; c < n-1; c++ {
+        for r := 0; r < m; r++ {
+            leftSum += grid[r][c]
+        }
+        if leftSum*2 == total {
+            return true
+        }
+    }
 
-	// check all vertical cuts
-	currentSum = 0
-	for c := 0; c < n; c++ {
-		currentSum += cols[c]
-		if total-currentSum == currentSum {
-			return true
-		}
-	}
-
-	return false
+    return false
 }
