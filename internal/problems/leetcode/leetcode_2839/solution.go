de37df993beb50b97bd742051d10a8d7d8965a74
Ihor Khlaponin <igor.boxmails@gmail.com>
2026-03-29 10:33:29 +0100

    leetcode 2839: check if strings can be made equal with operations I

---
+++ internal/problems/leetcode/leetcode_2839/solution.go
@@ -0,0 +1,23 @@
+package leetcode2839
+
+// https://leetcode.com/problems/check-if-strings-can-be-made-equal-with-operations-i/
+//
+// Time Complexity: O(1) since the length of s1 and s2 is fixed at 4
+// Space Complexity: O(1)
+func canBeEqual(s1 string, s2 string) bool {
+	chars := []rune(s2)
+	combinations := make(map[string]struct{}, 4)
+
+	for i := range 4 {
+		combinations[string(chars)] = struct{}{}
+		if i%2 == 0 {
+			chars[0], chars[2] = chars[2], chars[0]
+		} else {
+			chars[1], chars[3] = chars[3], chars[1]
+		}
+	}
+	if _, ok := combinations[s1]; ok {
+		return true
+	}
+	return false
+}
