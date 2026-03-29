77157dc78342ba234f6993184a0960c7b94dcd88
Ihor Khlaponin <igor.boxmails@gmail.com>
2026-03-29 10:57:53 +0100

    leetcode 2839: check if strings can be made equal with operations I

--- internal/problems/leetcode/leetcode_2839/solution.go
+++ internal/problems/leetcode/leetcode_2839/solution.go
@@ -18,3 +18,10 @@ func canBeEqual(s1 string, s2 string) bool {
 	}
 	return false
 }
+
+func canBeEqual_2(s1 string, s2 string) bool {
+	same := func(a, b, c, d byte) bool {
+		return (a == c && b == d) || (a == d && b == c)
+	}
+	return same(s1[0], s1[2], s2[0], s2[2]) && same(s1[1], s1[3], s2[1], s2[3])
+}
--- internal/problems/leetcode/leetcode_2839/solution_test.go
+++ internal/problems/leetcode/leetcode_2839/solution_test.go
@@ -29,3 +29,25 @@ func TestCanBeEqual(t *testing.T) {
 		})
 	}
 }
+
+func TestCanBeEqual2(t *testing.T) {
+	tests := []struct {
+		s1   string
+		s2   string
+		want bool
+	}{
+		{s1: "abcd", s2: "cdab", want: true},
+		{s1: "abcd", s2: "abcd", want: true},
+		{s1: "abcd", s2: "dacb", want: false},
+		{s1: "abcd", s2: "abdc", want: false},
+	}
+
+	for _, tt := range tests {
+		t.Run("", func(t *testing.T) {
+			got := canBeEqual_2(tt.s1, tt.s2)
+			if got != tt.want {
+				t.Errorf("canBeEqual_2(%q, %q) = %v, want %v", tt.s1, tt.s2, got, tt.want)
+			}
+		})
+	}
+}
