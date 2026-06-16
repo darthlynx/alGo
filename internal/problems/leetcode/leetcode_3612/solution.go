63ff9b3a2461459225c2466145887f147f837702
Ihor Khlaponin <igor.boxmails@gmail.com>
2026-06-16 23:43:59 +0100

    leetcode 3612: process strings with special operations I

--- internal/problems/leetcode/leetcode_3612/solution.go
+++ internal/problems/leetcode/leetcode_3612/solution.go
@@ -2,31 +2,36 @@ package leetcode3612
 
 // https://leetcode.com/problems/process-string-with-special-operations-i/
 //
-// Time Complexity: O(n),
-// Space Complexity: O(n).
+// Time Complexity: O(n + total length processed by '#' and '%' operations).
+// In the worst case, this can be O(2^n) due to repeated duplication.
+// Space Complexity: O(m), where m is the maximum generated string length.
 func processStr(s string) string {
 	stack := make([]byte, 0, len(s))
 
-	for _, ch := range s {
-		switch ch {
+	for i := 0; i < len(s); i++ {
+		switch s[i] {
 		case '*': // remove last
 			if len(stack) > 0 {
 				stack = stack[:len(stack)-1]
 			}
 		case '#': // duplicate current result
 			stack = append(stack, stack...)
-		case '%': // reverse
-			left := 0
-			right := len(stack) - 1
-			for left < right {
-				stack[left], stack[right] = stack[right], stack[left]
-				left++
-				right--
-			}
+		case '%':
+			reverse(stack)
 		default:
-			stack = append(stack, byte(ch))
+			stack = append(stack, byte(s[i]))
 		}
 	}
 
 	return string(stack)
+}
+
+func reverse(chars []byte) {
+	left := 0
+	right := len(chars) - 1
+	for left < right {
+		chars[left], chars[right] = chars[right], chars[left]
+		left++
+		right--
+	}
 }
